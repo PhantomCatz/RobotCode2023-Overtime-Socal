014a23db8d3a557cd3974ce88ab197414ae4d10a
infinitystraycat <hajuwon0601@gmail.com>
2023-09-22 16:10:20 -0700

    I integrated intake code to main.

--- OvertimeSoCal/src/main/java/frc/Mechanisms/CatzIntake.java
+++ OvertimeSoCal/src/main/java/frc/Mechanisms/CatzIntake.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
@@ -24,9 +23,6 @@ public class CatzIntake{
 
     private final int INTAKE_ROLLER_CAN_ID = 2637;
 
-    private final int GROUND_PCM_PORT    = 2;
-    private final int STOWED_PCM_PORT   = 5;
-
     private final SupplyCurrentLimitConfiguration currentLimit;
 
     private final double  CURRENT_LIMIT_AMPS            = 60.0;
@@ -34,6 +30,8 @@ public class CatzIntake{
     private final double  CURRENT_LIMIT_TIMEOUT_SECONDS = 0.5;
     private final boolean ENABLE_CURRENT_LIMIT          = true;
 
+    private final int GROUND_PCM_PORT    = 2;
+    private final int STOWED_PCM_PORT   = 5;
 
     private final double INTAKE_ROLLER_POWER = 1.0;
 
@@ -42,12 +40,10 @@ public class CatzIntake{
 
     private final int INTAKE_GROUND    = 1;
     private final int INTAKE_STOWED   = 0;
-    
+
     private int intakeState = INTAKE_STOWED;
 
-
-
-    public CatzIntake() {        
+    public CatzIntake() {
 
         intakeRoller =new WPI_TalonFX(INTAKE_ROLLER_CAN_ID);
 
@@ -58,7 +54,6 @@ public class CatzIntake{
         intakeRoller.configSupplyCurrentLimit(currentLimit);
 
         intakeSolenoid = new DoubleSolenoid(PCM_TYPE, GROUND_PCM_PORT, STOWED_PCM_PORT);
-
     }
 
     public boolean isStowed() 
@@ -75,28 +70,26 @@ public class CatzIntake{
         }
     }
 
-    public void cmdProcIntake(boolean getRightStickButton, double getLeftTriggerAxis, double getRightTriggerAxis)
+    public void cmdProcIntake(boolean toggleIntake, double intakeRollerInward, double intakeRollerOutward)
     {
-        if (getRightStickButton && isStowed())
+        if (toggleIntake && isStowed())
         {
             intakeSolenoid.set(Value.kForward);
             intakeState = INTAKE_GROUND;
         }
-        else if (getRightStickButton)
+        else if (toggleIntake)
         {
             intakeSolenoid.set(Value.kReverse);
             intakeState = INTAKE_STOWED;
         }
 
-        if (getRightTriggerAxis > 0.1)
+        if (intakeRollerInward > 0.1)
         {
             intakeRoller.set(ControlMode.PercentOutput, INTAKE_ROLLER_POWER);
         }
-        else if (getLeftTriggerAxis > 0.1)
+        else if (intakeRollerOutward > 0.1)
         {
             intakeRoller.set(ControlMode.PercentOutput, -INTAKE_ROLLER_POWER);
         }
     }
-
-    //didn't check if positive is inward in motors. Need to be fixed
 }
